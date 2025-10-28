fc8a487f4455901c27c88431e677c27723c92b0b
n-lovess <fwlm5667@leeds.ac.uk>
2025-10-28 12:22:15 +0000

    save

--- tasks/task15_2_3/src/Main.kt
+++ tasks/task15_2_3/src/Main.kt
@@ -2,4 +2,15 @@
 
 fun main() {
     // Write your main program here
+    val account = SavingsAccount("Nikki", 1.8)
+
+    account.deposit(1250)
+
+    repeat(5) {
+        account.applyInterest()
+    }
+
+    account.withdraw(50)
+
+    println("Final balance: £${account.balance}")
 }
--- tasks/task15_2_3/src/SavingsAccount.kt
+++ tasks/task15_2_3/src/SavingsAccount.kt
@@ -1,3 +1,13 @@
 // Task 15.2.3
 
 // Implement SavingsAccount class here
+class SavingsAccount(
+    holder: String,
+    val rate: Double
+) : BankAccount(holder) {
+
+    fun applyInterest() {
+        val interest = balance * (rate / 100)
+        deposit(interest.toInt())
+    }
+}
